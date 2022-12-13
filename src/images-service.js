9e8b9cf7071edf38773afa0a0f643da9c2f2386b
OlhaNekrylova <nekrylovaurist@gmail.com>
2022-12-13 19:18:38 +0200

    new NewApiService()

--- src/images-service.js
+++ src/images-service.js
@@ -1,25 +1,28 @@
 import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export default class NewApiService {
     constructor() {
         this.searchQuery = '';
         this.page = 1;
     }
-}
 
-async function getImages() {
-    const API_KEY = '32048668-e2fb2d2180cb5d2e63ce535ce';
-    const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&q=&{this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
-    const options = {
-        headers: {
-            Autorization: 'API_KEY';
+
+    async getImages() {
+        const API_KEY = '32048668-e2fb2d2180cb5d2e63ce535ce';
+        const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+        const options = {
+            headers: {
+                Autorization: 'API_KEY';
+            }
         }
-    }
 
-    try {
-        const response = await axios.get('${BASE_URL}');
-        console.log(response);
-    } catch (error) {
-        console.error(error);
+        try {
+            const response = await axios.get('${BASE_URL}');
+            console.log(response);
+        } catch (error) {
+            console.error(error);
+            Notify.failure('Sorry, there are no images matching your search query. Please try again');
+        }
     }
 }
--- src/search-images.js
+++ src/search-images.js
@@ -1,11 +1,21 @@
+import './sass/_common.scss';
+
+import NewApiService from './images-service';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const imagesApiService = new NewApiService();
 // import axios, {isCancel, AxiosError} from 'axios';
-
-
 // axios.get('/users')
 //   .then(res => {
 //     console.log(res.data);
 //   });
 // console.log(axios.isCancel('something'));
 
+function onSearch(evt) {
+    evt.preventDefault();
+    clearArticlesContainer();
+    imagesApiService.searchQuery = evt.currentTarget.elements.searchQuery.value;
+
+
+}
